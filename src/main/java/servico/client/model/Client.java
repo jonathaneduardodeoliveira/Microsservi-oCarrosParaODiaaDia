6646fdee6ee464c0974b7aa2a678ccd90d5139ee
Jonathan Eduardo de Oliveira <jonathan.mega2@gmail.com>
2024-11-12 21:06:38 -0300

    Criação da classe Client como entidade JPA com atributos id, name, cpf e habilitacao

---
+++ src/main/java/servico/client/model/Client.java
@@ -0,0 +1,73 @@
+package servico.client.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String habilitacao;
+
+    private LocalDateTime registrationDate;
+
+    public Client() {
+    }
+
+    public Client(String name, String cpf, String habilitacao, LocalDateTime registrationDate) {
+        this.name = name;
+        this.cpf = cpf;
+        this.habilitacao = habilitacao;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getHabilitacao() {
+        return habilitacao;
+    }
+
+    public void setHabilitacao(String habilitacao) {
+        this.habilitacao = habilitacao;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
